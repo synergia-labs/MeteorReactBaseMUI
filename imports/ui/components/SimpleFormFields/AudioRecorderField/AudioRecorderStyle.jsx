7c61e02cc6a04ab3ff19470eef44ea82f76fe380
aikaterhine <catarina.edp@gmail.com>
2021-01-06 11:57:28 -0300

    habilitando botao de stop e clique

--- imports/ui/components/SimpleFormFields/AudioRecorderField/AudioRecorderStyle.jsx
+++ imports/ui/components/SimpleFormFields/AudioRecorderField/AudioRecorderStyle.jsx
@@ -15,15 +15,17 @@
  */
 
 const audioRecorderStyle = {
-/*  example: {
-    color: 'rgba(0, 0, 0, 0.54)',
-    padding: 0,
-    fontSize: '1rem',
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    fontWeight: 400,
-    lineHeight: 1,
-    letterSpacing: '0.00938em',
-  },*/
+  containerRecord: {
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'left',
+    marginTop:16,
+    width:'100%',
+    marginBottom:16
+  },
+  buttonOptions: {
+    marginRight:3,
+  },
 };
 
 export {
