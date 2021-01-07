73d6d1fc8f7b00dbbd904ea90316c9613e21f19e
aikaterhine <catarina.edp@gmail.com>
2021-01-07 11:07:50 -0300

    editando largura das imagens

--- imports/ui/pages/Home/Home.tsx
+++ imports/ui/pages/Home/Home.tsx
@@ -4,21 +4,21 @@ import Container from '@material-ui/core/Container';
 
 const Home = () => (
   <>
-      <Container style={{maxWidth: '100%', maxHeight: '100%', boxSizing: 'border-box'}}>
+      <Container>
           <h1>Material-UI Template</h1>
           <p>This is a basic fixed menu template using fixed size containers.</p>
           <p>
               A text container is used for the main container, which is useful for single column layouts.
           </p>
 
-          <div style={{maxWidth: '100%', maxHeight: '100%', boxSizing: 'border-box'}}>
-            <img src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <div>
+            <img src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
+            <img src='/images/wireframe/paragraph.png' style={{ marginTop: '2em', maxWidth: '100%', maxHeight: '100%'}} />
           </div>
       </Container>
 
