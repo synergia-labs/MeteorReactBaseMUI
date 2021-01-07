9441a228849de5a5a8e8b01f0932f7a7cc456266
aikaterhine <catarina.edp@gmail.com>
2021-01-07 12:05:14 -0300

    ajustando erro na home

--- imports/ui/pages/Home/Home.tsx
+++ imports/ui/pages/Home/Home.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 
 import Container from '@material-ui/core/Container';
-import {appStyles} from "/imports/ui/theme/styles";
+import Typography from '@material-ui/core/Typography';
+
+import {homeStyles} from "./HomeStyle";
 
 const Home = () => (
   <>
       <Container>
-          <h1>Material-UI Template</h1>
+          <Typography style={homeStyles.title}>{'Material-UI Template'}</Typography>
           <p>This is a basic fixed menu template using fixed size containers.</p>
           <p>
               A text container is used for the main container, which is useful for single column layouts.
           </p>
 
-          <img src='/images/wireframe/media-paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
-          <img src='/images/wireframe/paragraph.png' style={appStyles.containerHome} />
+          <img src='/images/wireframe/media-paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
+          <img src='/images/wireframe/paragraph.png' style={homeStyles.containerHome} />
       </Container>
 
   </>
---
+++ imports/ui/pages/Home/HomeStyle.jsx
@@ -0,0 +1,36 @@
+/*!
+
+ =========================================================
+ * Material Dashboard React - v1.0.0 based on Material Dashboard - v1.2.0
+ =========================================================
+
+ * Product Page: http://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2018 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+
+import { max } from "lodash";
+import {isMobile} from "/imports/libs/deviceVerify";
+
+const homeStyles = {
+  title: {
+      fontSize: isMobile?'20px':'30px',
+      paddingTop: isMobile?'20px':'20px',
+      paddingBottom: isMobile?'20px':'20px',
+      fontWeight: isMobile?'500':'500',
+  },
+  containerHome: {
+     marginTop: '2em',
+     maxWidth: '100%',
+     maxHeight: '100%',
+  }
+};
+
+export {
+    homeStyles,
+};
