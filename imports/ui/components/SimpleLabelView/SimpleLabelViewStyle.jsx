d2015f01d0c4905f2bc55d929937cb19cf2dd4c5
MarceloSG <marcelosilveira@dcc.ufmg.br>
2021-01-13 10:37:56 -0300

    font size label

--- imports/ui/components/SimpleLabelView/SimpleLabelViewStyle.jsx
+++ imports/ui/components/SimpleLabelView/SimpleLabelViewStyle.jsx
@@ -5,7 +5,7 @@ const simpleLabelStyle = {
   displayLabel: {
     color: 'rgba(0,0,0,0.54)',
     padding: 0,
-    fontSize: '11px',
+    fontSize: '1rem',
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     fontWeight: 400,
     lineHeight: 1,
