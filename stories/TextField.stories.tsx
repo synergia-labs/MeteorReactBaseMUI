608e205ed8a1ba5378ccc4903d323d2c8268bedd
catarina enya <catarina.edp@gmail.com>
2021-01-22 11:46:42 -0300

    adicionando campos ao comp

--- stories/TextField.stories.tsx
+++ stories/TextField.stories.tsx
@@ -3,21 +3,36 @@
 import React, { ComponentProps } from 'react';
 import { Story } from '@storybook/react/types-6-0';
 
-import TextField from '/imports/ui/components/SimpleFormFields/TextField/TextField';
+import { TextField, TextFieldProps } from './TextField';
 
 // This default export determines where your story goes in the story list
 export default {
   title: 'TextField',
   component: TextField,
+  args: {
+    disabled: false,
+    required: false,
+  },
+  argTypes: {
+  },
 };
 
-const Template: Story<ComponentProps<typeof TextField>> = (args) => (
-  <TextField {...args} />
-);
+const Template: Story<TextFieldProps> = (args) => <TextField {...args} />;
 
-export const FirstStory = Template.bind({});
-FirstStory.args = {
+export const DefaultStory = Template.bind({});
+DefaultStory.args = {
   /* the args you need here will depend on your component */
-  label: 'hello',
-  borderWidth: 1,
+  placeholder: 'Título',
+};
+DefaultStory.argTypes = {
+  /* the args you need here will depend on your component */
+};
+
+export const PasswordStory = Template.bind({});
+PasswordStory.args = {
+  /* the args you need here will depend on your component */
+  placeholder: 'Senha',
+  type: "password",
+};
+PasswordStory.argTypes = {
 };
