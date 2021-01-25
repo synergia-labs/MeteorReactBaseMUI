04adf30cca89cfe1013331cdc44b49f0227a4a68
Victoria <victoriarsaliba@gmail.com>
2021-01-25 08:30:01 -0300

    chip input no storybook

---
+++ stories/ChipInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './button.css';
+
+import ChipInputComp from '/imports/ui/components/SimpleFormFields/ChipInput/ChipInput';
+
+export interface ChipInputProps {
+  placeholder: string;
+  name: string;
+  disabled?: boolean;
+  required?: boolean;
+  type?:string;
+}
+/**
+ * Primary UI component for user interaction
+ */
+export const ChipInput: React.FC<ChipInputProps> = ({
+  placeholder,
+  name,
+  disabled=false,
+  required=false,
+  type,
+  ...props
+}) => {
+  return (
+    <ChipInputComp
+      name={name}
+      type={type}
+      {...props}
+    />
+  );
+};
