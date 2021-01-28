c8b624a7880fe9b10f9ad50c4743f977754bf666
catarina enya <catarina.edp@gmail.com>
2021-01-28 09:38:14 -0300

    simpleform

--- stories/SimpleForm/SimpleForm.tsx
+++ stories/SimpleForm/SimpleForm.tsx
@@ -14,133 +14,185 @@ import { ToggleField } from '../ToggleField/ToggleField';
 import { SimpleImageUploadBase64 } from '../UploadImage/SimpleImageUploadBase64';
 
 export interface SimpleFormProps {
-  placeholder: string;
-  name: string;
-  disabled?: boolean;
-  required?: boolean;
-  options: [Object];
-  backgroundColor?: string;
-  color?: string;
-  image: [Object];
-  title: [Object];
-  description: [Object];
-  type: [Object];
-  date: [Object];
-  chip: [Object];
-  contacts: [Object];
-  audio: [Object];
-  address: [Object];
-  statusRadio: [Object];
-  statusToggle: [Object];
+  mode?: String;
+  schema?: [Object];
+  doc?: [Object];
+  onSubmit?: [Object];
+  loading?: boolean;
 }
+
+const elements = {
+  image: {
+    label: 'Imagem',
+    placeholder: 'Imagem',
+    name: 'Imagem',
+  },
+  title: {
+    label: 'Título',
+    placeholder: 'Título',
+    name: 'Título',
+  },
+  description: {
+    label: 'Descrição',
+    placeholder: 'Descrição',
+    name: 'Descrição',
+  },
+  type: {
+    placeholder: 'Tipo',
+    name: 'Tipo',
+    options: [
+        {value:'normal', label:'Normal'},
+        {value:'extra', label:'Extra'},
+    ],
+  },
+  date: {
+    label: 'Data',
+    placeholder: 'Data',
+    name: 'Data',
+  },
+  chip: {
+    label: 'Chip Input',
+    placeholder: 'Chip Input',
+    name: 'Chip Input',
+  },
+  contacts: {
+    type: Object,
+    label: 'Contatos',
+    placeholder: 'Contatos',
+    name: 'Contatos',
+    mask: {
+      phone: {
+        label: 'Telefone',
+        placeholder: 'Telefone',
+        name: 'Telefone',
+        mask : '(##) ####-####',
+      },
+      cpf: {
+        label: 'CPF',
+        placeholder: 'CPF',
+        name: 'CPF',
+        mask : '###.###.###-##',
+      },
+    }
+  },
+  audio: {
+    label: 'Audio',
+    placeholder: 'Audio',
+    name: 'Audio',
+  },
+  address: {
+    label: 'Localização',
+    placeholder: 'Localização',
+    name: 'Localização',
+  },
+  statusRadio: {
+    label: 'Status Radio',
+    placeholder: 'Status Radio',
+    name: 'Status Radio',
+  },
+  statusToggle: {
+    label: 'Status Toogle',
+    placeholder: 'Status Toogle',
+    name: 'Status Toogle',
+  },
+};
 
 /**
  * Primary UI component for user interaction
  */
 export const SimpleForm: React.FC<SimpleFormProps> = ({
-  disabled=false,
-  required=false,
-  options=[],
-  backgroundColor,
-  color,
-  image,
-  title,
-  description,
-  type,
-  date,
-  chip,
-  contacts,
-  audio,
-  address,
-  statusRadio,
-  statusToggle,
+  mode={view},
+  schema= [exampleApi.schema],
+  doc= [exampleDoc],
+  onSubmit= [handleSubmit],
+  loading={true},
   ...props
 }) => {
 
   return (
-    <div style={{ backgroundColor, color, display: 'flex', flexDirection: 'column' }}>
+    <div style={{display: 'flex', flexDirection: 'column' }}>
       <SimpleImageUploadBase64
-        placeholder={image.placeholder}
-        label={image.name}
-        disabled={image.disabled}
-        required={image.required}
+        placeholder={elements.image.placeholder}
+        label={elements.image.name}
+        disabled={elements.image.disabled}
+        required={elements.image.required}
         {...props}
       />
       <TextField
-        placeholder={title.placeholder}
-        label={title.name}
-        disabled={title.disabled}
-        required={title.required}
+        placeholder={elements.title.placeholder}
+        label={elements.title.name}
+        disabled={elements.title.disabled}
+        required={elements.title.required}
         {...props}
       />
       <TextField
-        placeholder={description.placeholder}
-        label={description.name}
-        disabled={description.disabled}
-        required={description.required}
+        placeholder={elements.description.placeholder}
+        label={elements.description.name}
+        disabled={elements.description.disabled}
+        required={elements.description.required}
         {...props}
       />
       <SelectField
-        placeholder={type.placeholder}
-        label={type.name}
-        disabled={type.disabled}
-        required={type.required}
+        placeholder={elements.type.placeholder}
+        label={elements.type.name}
+        disabled={elements.type.disabled}
+        required={elements.type.required}
         {...props}
       />
       <DatePickerField
-        placeholder={date.placeholder}
-        label={date.name}
-        disabled={date.disabled}
-        required={date.required}
+        placeholder={elements.date.placeholder}
+        label={elements.date.name}
+        disabled={elements.date.disabled}
+        required={elements.date.required}
         {...props}
       />
       <ChipInput
-        placeholder={chip.placeholder}
-        label={chip.name}
-        disabled={chip.disabled}
-        required={chip.required}
+        placeholder={elements.chip.placeholder}
+        label={elements.chip.name}
+        disabled={elements.chip.disabled}
+        required={elements.chip.required}
         {...props}
       />
       <TextMaskField
-        placeholder={contacts.mask.phone.placeholder}
-        label={contacts.mask.phone.name}
-        disabled={contacts.mask.phone.disabled}
-        required={contacts.mask.phone.required}
+        placeholder={elements.contacts.mask.phone.placeholder}
+        label={elements.contacts.mask.phone.name}
+        disabled={elements.contacts.mask.phone.disabled}
+        required={elements.contacts.mask.phone.required}
         {...props}
       />
       <TextMaskField
-        placeholder={contacts.mask.cpf.placeholder}
-        label={contacts.mask.cpf.name}
-        disabled={contacts.mask.cpf.disabled}
-        required={contacts.mask.cpf.required}
+        placeholder={elements.contacts.mask.cpf.placeholder}
+        label={elements.contacts.mask.cpf.name}
+        disabled={elements.contacts.mask.cpf.disabled}
+        required={elements.contacts.mask.cpf.required}
         {...props}
       />
       <AudioRecorder
-        placeholder={audio.placeholder}
-        label={audio.name}
-        disabled={audio.disabled}
-        required={audio.required}
+        placeholder={elements.audio.placeholder}
+        label={elements.audio.name}
+        disabled={elements.audio.disabled}
+        required={elements.audio.required}
         {...props}
       />
       <GoogleApiWrapper
-        placeholder={address.placeholder}
-        label={address.name}
-        disabled={address.disabled}
-        required={address.required}
+        placeholder={elements.address.placeholder}
+        label={elements.address.name}
+        disabled={elements.address.disabled}
+        required={elements.address.required}
         {...props}
       />
       <RadioButtonField
-        placeholder={statusRadio.placeholder}
-        label={statusRadio.name}
-        disabled={statusRadio.disabled}
-        required={statusRadio.required}
+        placeholder={elements.statusRadio.placeholder}
+        label={elements.statusRadio.name}
+        disabled={elements.statusRadio.disabled}
+        required={elements.statusRadio.required}
         {...props}
       />
       <ToggleField
-        placeholder={statusToggle.placeholder}
-        label={statusToggle.name}
-        disabled={statusToggle.disabled}
-        required={statusToggle.required}
+        placeholder={elements.statusToggle.placeholder}
+        label={elements.statusToggle.name}
+        disabled={elements.statusToggle.disabled}
+        required={elements.statusToggle.required}
         {...props}
       />
     </div>
