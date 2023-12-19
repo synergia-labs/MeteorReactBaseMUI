3db0d76ab1a0319cfd7d2bf068bc8d77a49035dc
Gustavo Barcelos <gustavo05rb@gmail.com>
2023-12-19 14:36:20 -0300

    :package: feat: Adicionando novo módulo: StoryBook

---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import {getTheme} from '../imports/materialui/theme';
+
+export const decorators = [
+	withThemeFromJSXProvider({
+	themes: {
+	  light: getTheme({darkMode: false, fontScale: 1, isMobile: false}),
+	  dark: getTheme({darkMode: true, fontScale: 1, isMobile: false})
+	},
+	defaultTheme: 'light',
+	Provider: ThemeProvider,
+	GlobalStyles: CssBaseline,
+  })];
+
+export const parameters = {
+actions: { argTypesRegex: '^on[A-Z].*' },
+controls: {
+	expanded: true, // Adds the description and default columns
+	matchers: {
+	color: /(background|color)$/i,
+	date: /Date$/,
+	},
+},
+};
