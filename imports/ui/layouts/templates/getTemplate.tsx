fa39f69518ee38e7719f36b4990f36c0f1e2c3ba
Gustavo Barcelos <gustavo05rb@gmail.com>
2024-01-10 18:53:55 -0300

    :books: docs: Adição do diagrama de templates para documentação

--- imports/ui/layouts/templates/getTemplate.tsx
+++ imports/ui/layouts/templates/getTemplate.tsx
@@ -13,7 +13,15 @@ const templates = {
 };
 
 export interface ISysTemplate {
+    /**Propriedade que define qual template será renderizado. 
+     * 
+     * **obs:** Essa propriedade é usada como atributo `template` na definição da rota.
+    */
     variant: SysTemplateOptions | keyof typeof SysTemplateOptions;
+    /**Propriedades que podem ser passadas por parâmetro para o template. 
+     * 
+     * **obs:** Essa propriedade é usada como atributo `templateProps` na definição da rota.
+    */
     props?: any;
     children?: React.ReactNode;
 }
