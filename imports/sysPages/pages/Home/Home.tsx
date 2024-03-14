9f639b1b4e1e31776d4407eb537fcfa917b243fb
Gabriel <gabrielsaturnino4@gmail.com>
2024-03-14 17:51:51 -0300

    voltando a home

--- imports/sysPages/pages/Home/Home.tsx
+++ imports/sysPages/pages/Home/Home.tsx
@@ -4,37 +4,21 @@ import { Typography } from '@mui/material';
 import HomeSectionNotificacoes from './sections/notificacoes';
 import HomeSectionDialogs from './sections/dialogs';
 
-import { Loading } from '/imports/ui/components/Loading/Loading';
-
 const Home: React.FC = () => {
-	const [loads, setLoad] = React.useState(true);
-
-	React.useEffect(() => {
-		setTimeout(() => {
-			setLoad(false);
-		}, 5000);
-	}, []);
-
 	return (
-		<>
-			{loads ? (
-				<Loading />
-			) : (
-				<HomePageContainer>
-					<HomePageHeader>
-						<Typography variant="h3">Página de testes</Typography>
-						<Typography variant="body1">
-							Bem vindo ao Boilerplate do Synergia. Essa é uma página dedicada aos testes e exibições de componentes e
-							funcionalidades do nosso sistema. Esperamos que você aproveite e aprenda bastante com ela. Para mais
-							dúvidas consulte nossa documentação oficial pelo storybook.
-						</Typography>
-					</HomePageHeader>
+		<HomePageContainer>
+			<HomePageHeader>
+				<Typography variant="h3">Página de testes</Typography>
+				<Typography variant="body1">
+					Bem vindo ao Boilerplate do Synergia. Essa é uma página dedicada aos testes e exibições de componentes e
+					funcionalidades do nosso sistema. Esperamos que você aproveite e aprenda bastante com ela. Para mais dúvidas
+					consulte nossa documentação oficial pelo storybook.
+				</Typography>
+			</HomePageHeader>
 
-					<HomeSectionNotificacoes />
-					<HomeSectionDialogs />
-				</HomePageContainer>
-			)}
-		</>
+			<HomeSectionNotificacoes />
+			<HomeSectionDialogs />
+		</HomePageContainer>
 	);
 };
 
