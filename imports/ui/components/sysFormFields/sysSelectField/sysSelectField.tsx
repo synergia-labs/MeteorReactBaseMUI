ecc2eb858afd51941bdd809034a41d1d7a1939a5
Victor Ferreira <victorferreira.contacto@gmail.com>
2024-03-28 12:15:41 -0300

    feat: tipagem do SysSelectField

--- imports/ui/components/sysFormFields/sysSelectField/sysSelectField.tsx
+++ imports/ui/components/sysFormFields/sysSelectField/sysSelectField.tsx
@@ -45,7 +45,7 @@ export const SysSelectField: React.FC<ISysSelectFieldProps> = ({
 	disabled = disabled || sysFormController?.disabled;
 	error = error || sysFormController?.error;
 	readOnly = readOnly || sysFormController?.readOnly;
-	options = options || sysFormController?.schema?.options || [];
+	options = options || sysFormController?.schema?.options || ([] as any);
 	multiple = multiple || sysFormController?.schema?.multiple;
 
 	const [valueText, setValueText] = useState(defaultValue);
@@ -57,29 +57,14 @@ export const SysSelectField: React.FC<ISysSelectFieldProps> = ({
 	};
 
 	if (readOnly) {
-		const viewValue = options.find((option) => option.value === valueText)?.label;
+		const viewValue = options?.find((option) => option.value === valueText)?.label;
 		return <SysViewField label={label} placeholder={viewValue || '-'} />;
 	}
-	// function onFieldChange(e: React.BaseSyntheticEvent) {
-	// 	const newValue = e.target.value;
-	// 	if (!!max && newValue.length > max) return;
-	// 	if (mask) {
-	// 		const inputValue = generalMask(newValue, mask);
-	// 		const transformedValue = removerFormatacoes(inputValue);
-	// 		setValueText(inputValue);
-	// 		sysFormController?.onChange({ name, value: transformedValue });
-	// 	} else {
-	// 		setValueText(newValue);
-	// 		sysFormController?.onChange({ name, value: newValue });
-	// 	}
-	// }
 
 	return (
 		<SysLabelView label={label}>
 			<Select labelId={`${label}${name}`} id={name} value={valueText} onChange={handleChange}>
-				{options.map((option) => (
-					<MenuItem value={option.value}>{option.label}</MenuItem>
-				))}
+				{options?.map((option) => <MenuItem value={option.value}>{option.label}</MenuItem>)}
 			</Select>
 		</SysLabelView>
 	);
