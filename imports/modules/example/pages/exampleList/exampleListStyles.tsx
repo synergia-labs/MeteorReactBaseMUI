5df8d7633ac01f97f42aaa9abb74a1e61b9664c8
Gustavo Barcelos <gustavo05rb@gmail.com>
2024-04-05 11:26:16 -0300

    :lipstick: style: Ajustes e padronizações no ExampleList

--- imports/modules/example/pages/exampleList/exampleListStyles.tsx
+++ imports/modules/example/pages/exampleList/exampleListStyles.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import {Box, styled} from "@mui/material";
+import React, { ElementType } from "react";
+import {Box, BoxProps, styled} from "@mui/material";
 import { sysSizing } from "/imports/ui/materialui/styles";
 
-export const ExampleListViewStyledContainer = styled(Box)(() => ({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    width: '100%',
-    padding: `${sysSizing.contentPt} ${sysSizing.contentPx}`,
-    gap: sysSizing.spacingFixedMd,
-}));
+
+interface IExampleListStyles {
+    container: ElementType<BoxProps>;
+}
+
+const ExampleListStyles: IExampleListStyles = {
+    container: styled(Box)(({theme}) => ({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        width: '100%',
+        padding: `${sysSizing.contentPt} ${sysSizing.contentPx}`,
+        gap: sysSizing.spacingFixedMd,
+        transition: 'all 0.3s ease',
+        [theme.breakpoints.down('sm')]: {
+            padding: `${sysSizing.contentPt} ${sysSizing.spacingFixedMd}`,
+        }
+    }))
+}
+
+export default ExampleListStyles;
