c6a1d9b575615e2c9842fb8c95ed79313007c9e3
Gustavo Barcelos <gustavo05rb@gmail.com>
2024-04-30 23:00:13 -0300

    :bug: fix: Correção do tratamento de erros da página de login

--- imports/sysPages/pages/signIn/signIn.tsx
+++ imports/sysPages/pages/signIn/signIn.tsx
@@ -1,4 +1,3 @@
-import { Box, Button, Typography } from '@mui/material';
 import React, { useContext, useEffect } from 'react';
 import SignInStyles from './signInStyles';
 import { Meteor } from 'meteor/meteor';
@@ -10,74 +9,73 @@ import SysTextField from '/imports/ui/components/sysFormFields/sysTextField/sysT
 import SysForm from '/imports/ui/components/sysForm/sysForm';
 import SysFormButton from '/imports/ui/components/sysFormFields/sysFormButton/sysFormButton';
 import { signInSchema } from './signinsch';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const SignInPage: React.FC = () => {
-	const { showNotification } = useContext(SysAppLayoutContext);
-	const { user } = useContext(SysAppContext);
-	const navigate = useNavigate();
+  const { showNotification } = useContext(SysAppLayoutContext);
+  const { user } = useContext(SysAppContext);
+  const navigate = useNavigate();
 
-	const handleCallback = (err?: Error | Meteor.Error | Meteor.TypedError | undefined) => {
-		const { reason } = err as Meteor.Error;
-		if (!err) return;
+  const handleSubmit = ({ email, password }: { email: string; password: string }) =>
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (!err) return navigate('/');
+      if ((err as Meteor.Error).reason === 'User not found')
+        return showNotification({
+          type: 'error',
+          title: 'Não foi possível realizar o login',
+          message: 'Este email não está cadastrado em nossa base de dados.'
+        });
+      showNotification({
+        type: 'error',
+        title: 'Não foi possível realizar o login',
+        message: (err as Meteor.Error).reason || (err as Meteor.Error).message || 'Erro desconhecido'
+      });
 
-		showNotification({
-			type: 'error',
-			title: 'Acesso negado!',
-			message:
-				reason === 'Incorrect password'
-					? 'Email ou senha inválidos'
-					: reason === 'User not found'
-						? 'Este email não está cadastrado em nossa base de dados.'
-						: ''
-		});
-	};
+    });
 
-	const handleSubmit = ({ email, password }: { email: string; password: string }) => {
-		Meteor.loginWithPassword(email, password, (err) => handleCallback(err));
-	};
+  const handleForgotPassword = () => navigate('/password-recovery');
 
-	const handleForgotPassword = () => {
-		navigate('/password-recovery');
-	};
 
-	useEffect(() => {
-		if (user) navigate('/');
-	}, [user]);
+  useEffect(() => {
+    if (user) navigate('/');
+  }, [user]);
 
-	return (
-		<SignInStyles.container>
-			<SignInStyles.content>
-				<Typography variant="h1" display={'inline-flex'} gap={1}>
-					<Typography variant="inherit" color={(theme) => theme.palette.sysText?.tertiary}>
-						{'{'}
-					</Typography>
-					Boilerplate
-					<Typography variant="inherit" color="sysText.tertiary">
-						{'}'}
-					</Typography>
-				</Typography>
+  return (
+    <SignInStyles.container>
+      <SignInStyles.content>
+        <Typography variant="h1" display={'inline-flex'} gap={1}>
+          <Typography variant="inherit" color={(theme) => theme.palette.sysText?.tertiary}>
+            {'{'}
+          </Typography>
+          Boilerplate
+          <Typography variant="inherit" color="sysText.tertiary">
+            {'}'}
+          </Typography>
+        </Typography>
 
-				<SignInStyles.formContainer>
-					<Typography variant="h5">Acesse o sistema</Typography>
-					<SysForm schema={signInSchema} onSubmit={handleSubmit} debugAlerts={false}>
-						<SignInStyles.formWrapper>
-							<SysTextField name="email" label="Email" fullWidth placeholder="Digite seu email" />
-							<SysTextField label="Senha" fullWidth name="password" placeholder="Digite sua senha" type="password" />
-							<Button variant="text" sx={{ alignSelf: 'flex-end' }} onClick={handleForgotPassword}>
-								<Typography variant="link">Esqueci minha senha</Typography>
-							</Button>
-							<Box />
-							<SysFormButton variant="contained" color="primary" endIcon={<ArrowForwardRoundedIcon />}>
-								Entrar
-							</SysFormButton>
-						</SignInStyles.formWrapper>
-					</SysForm>
-				</SignInStyles.formContainer>
+        <SignInStyles.formContainer>
+          <Typography variant="h5">Acesse o sistema</Typography>
+          <SysForm schema={signInSchema} onSubmit={handleSubmit} debugAlerts={false}>
+            <SignInStyles.formWrapper>
+              <SysTextField name="email" label="Email" fullWidth placeholder="Digite seu email" />
+              <SysTextField label="Senha" fullWidth name="password" placeholder="Digite sua senha" type="password" />
+              <Button variant="text" sx={{ alignSelf: 'flex-end' }} onClick={handleForgotPassword}>
+                <Typography variant="link">Esqueci minha senha</Typography>
+              </Button>
+              <Box />
+              <SysFormButton variant="contained" color="primary" endIcon={<ArrowForwardRoundedIcon />}>
+                Entrar
+              </SysFormButton>
+            </SignInStyles.formWrapper>
+          </SysForm>
+        </SignInStyles.formContainer>
 
-				<Box component="img" src="/images/wireframe/synergia-logo.svg" sx={{ width: '100%', maxWidth: '400px' }} />
-			</SignInStyles.content>
-		</SignInStyles.container>
-	);
+        <Box component="img" src="/images/wireframe/synergia-logo.svg" sx={{ width: '100%', maxWidth: '400px' }} />
+      </SignInStyles.content>
+    </SignInStyles.container>
+  );
 };
 
 export default SignInPage;
