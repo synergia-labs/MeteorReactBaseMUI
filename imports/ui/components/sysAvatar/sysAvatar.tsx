1e4ccb8087a7c401ecf108bfe579aa9c16a19a38
Gustavo Barcelos <gustavo05rb@gmail.com>
2025-02-08 09:44:34 -0300

    :recycle: refactor: Alterações no compoente 'sysAvatar'

--- imports/ui/components/sysAvatar/sysAvatar.tsx
+++ imports/ui/components/sysAvatar/sysAvatar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { AvatarProps } from '@mui/material/Avatar';
-import { SxProps, Theme } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import Styles from './sysAvatarStyles';
+import {AvatarProps} from '@mui/material/Avatar';
+import Typography, { TypographyProps } from '@mui/material/Typography';
+import {SxProps, Theme} from '@mui/material/styles';
+import { hasValue } from '/imports/libs/hasValue';
 
-import SysAvatarStyles from './sysAvatarStyles';
-import { hasValue } from '../../../libs/hasValue';
-
-const { Container, Avatar } = SysAvatarStyles;
-
-export interface SysAvatarProps extends Omit<AvatarProps, 'onCLick'> {
-	/**O nome que será usado para mostrar a primeira letra no avatar.*/
-	name?: string;
-	/**Estilos personalizados para o componente Box que envolve o Avatar.*/
-	backgroundSx?: SxProps<Theme>;
-	/**A cor da borda do avatar.*/
-	borderColor?: string;
-	/**Um manipulador de eventos onClick para o componente.*/
-	onClick?: (event?: any) => void;
+interface ISysAvatar extends Omit<AvatarProps, 'onClick'> {
+  /**O nome que será usado para mostrar a primeira letra no avatar.*/
+  name?: string;
+  /**Tamanhos default para o componente @default medium*/
+  size?: 'small' | 'medium' | 'large';
+  /** Borda circular de distância de 4 px do avatar. @default false */
+  activateOutline?: boolean;
+  /** Estilos personalizados para o componente Box que envolve o Avatar. */
+  sxAvatar?: SxProps<Theme>;
+  /** Variante do texto que exibe a letra do nome no avatar. */
+  textVariant?: TypographyProps['variant'];
+  /** Função chamada quando há um click no componente */
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
 /**
@@ -27,12 +28,30 @@ export interface SysAvatarProps extends Omit<AvatarProps, 'onCLick'> {
  * - O componente `SysAvatar` é um componente React personalizado que exibe um avatar.
  * - O componente prioriza a exibição da imagem através da propriedade `src` e, caso não seja possível, exibe a primeira letra do nome através da propriedade `name`.
  */
-export const SysAvatar: React.FC<SysAvatarProps> = ({ name, backgroundSx, borderColor, onClick, ...props }) => {
-	return (
-		<Container sx={backgroundSx} onClick={onClick} activeOnClick={hasValue(onClick)} borderColor={borderColor}>
-			<Avatar {...props} tabIndex={0}>
-				<Typography variant="h3">{name?.[0].toUpperCase()}</Typography>
-			</Avatar>
-		</Container>
-	);
+const SysAvatar: React.FC<ISysAvatar> = ({
+  name,
+  onClick,
+  activateOutline = false,
+  size = 'medium',
+  sx,
+  sxAvatar,
+  textVariant = 'h5',
+  ...otherProps
+}) => {
+  return (
+    <Styles.container 
+      size={size} 
+      onClick={onClick}
+      activateOutline={activateOutline}
+      cursorPointer={hasValue(onClick)}
+      sx={sx}
+    >
+      <Styles.avatar {...otherProps} sx={sxAvatar}>
+        <Typography variant={textVariant}>{name?.[0].toUpperCase()}</Typography>
+      </Styles.avatar>
+    </Styles.container>
+  );
 };
+
+
+export default SysAvatar;
--- imports/ui/components/sysAvatar/sysAvatarStyles.tsx
+++ imports/ui/components/sysAvatar/sysAvatarStyles.tsx
@@ -1,51 +1,53 @@
-import React, { ElementType } from 'react';
-import { Avatar, AvatarProps, Box, BoxProps, styled, Theme } from '@mui/material';
+import React, {ElementType} from 'react';
+import Box, {BoxProps} from '@mui/material/Box';
+import styled from '@mui/material/styles/styled';
+import Avatar, {AvatarProps} from '@mui/material/Avatar';
 import { sysSizing } from '../../materialui/styles';
 
-interface ISysAvatarStyles {
-	Container: ElementType<BoxProps & IContainer>;
-	Avatar: ElementType<AvatarProps>;
-}
 
-interface IContainer {
-	activeOnClick?: boolean;
-	borderColor?: string;
+interface IContainer extends BoxProps {
+  cursorPointer?: boolean;
+  size: 'small' | 'medium' | 'large';
+  activateOutline: boolean;
+}
+interface ISysAvatarStyles {
+  container: ElementType<IContainer>;
+  avatar: ElementType<AvatarProps>;
 }
 
 const SysAvatarStyles: ISysAvatarStyles = {
-	Container: styled(({ activeOnClick, borderColor, ...otherProps }: IContainer) => <Box {...otherProps} />)<IContainer>(
-		({ theme, activeOnClick, borderColor }: { theme?: Theme; activeOnClick?: boolean; borderColor?: string }) => ({
-			width: sysSizing.componentsButtonMediumMinHeight,
-			height: sysSizing.componentsButtonMediumMinHeight,
-			borderRadius: '50%',
-			border: `2px solid ${borderColor ?? theme?.palette.common.white}`,
-			boxSizing: 'border-box',
-			display: 'flex',
-			overflow: 'hidden',
-			alignItems: 'center',
-			justifyContent: 'center',
-			padding: sysSizing.componentsInputMinHeight,
-			cursor: activeOnClick ? 'pointer' : 'default',
-			flexShrink: 0
-		})
-	),
-	Avatar: styled(Avatar)(({ theme }) => ({
-		width: sysSizing.contentPt,
-		height: sysSizing.contentPt,
-		backgroundColor: theme?.palette.primary.dark,
-		transition: 'all 150ms linear',
-		'&:hover': {
-			backgroundColor: theme?.palette.sysAction?.primaryContrastText,
-			color: theme.palette.sysAction?.primaryHover,
-			transition: 'all 150ms linear'
-		},
-		'&:focus': {
-			backgroundColor: theme?.palette.sysAction?.primaryContrastText,
-			color: theme?.palette.sysAction?.primaryHover,
-			outline: 'none',
-			transition: 'all 150ms linear'
-		}
-	}))
+  container: styled(Box, {
+    shouldForwardProp: (prop) => prop !== 'cursorPointer' && prop !== 'size' && prop !== 'activateOutline'
+  })<IContainer>(({ theme, cursorPointer, size, activateOutline }) => ({
+    width: size === 'small' ? sysSizing.componentsButtonSmallMinHeight : size === 'medium' ? sysSizing.spacingRemXl : sysSizing.componentsButtonMediumMinHeight,
+    height: size === 'small' ? sysSizing.componentsButtonSmallMinHeight : size === 'medium' ? sysSizing.spacingRemXl : sysSizing.componentsButtonMediumMinHeight,
+    borderRadius: '50%',
+    border: !activateOutline ? 'none' : `2px solid ${theme?.palette.common.white}`,
+    display: 'flex',
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: !activateOutline ? 'none' : sysSizing.spacingFixedXs,
+    cursor: cursorPointer ? 'pointer' : 'inherit',
+    flexShrink: 0, 
+    minWidth: activateOutline ? sysSizing.componentsButtonMediumMinHeight : 'auto',
+    minHeight: activateOutline ? sysSizing.componentsButtonMediumMinHeight : 'auto',
+    '& .MuiAvatar-root': {
+      margin: 0
+    }
+  })),
+  avatar: styled(Avatar)(({theme}) => ({
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme?.palette.primary.dark,
+    flexShrink: 0,
+    marigin: 0,
+    '&:focus': {
+      backgroundColor: theme?.palette.sysAction?.primaryContrastText,
+      color: theme?.palette.sysAction?.primaryHover,
+      outline: 'none'
+    }
+  }))
 };
 
 export default SysAvatarStyles;
