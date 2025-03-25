96ed25fd1fc0c494c73dcca8f98b1df1a3e1b064
Gabriel Rosa <ms.gabriel.rosa@gmail.com>
2025-03-25 12:47:26 -0300

    feature: module AutoCreate

--- .module/new-module.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define caminhos dos diretórios
-SOURCE_DIR=".module"
-DEST_DIR="imports/modules"
-
-# Templates de módulos disponíveis
-templates=("simple")
--- imports/modules/example/frontend/pages/context.ts
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-
-interface IExampleModuleContext {}
-
-const ExampleModuleContext = createContext<IExampleModuleContext>({});
-
-export default ExampleModuleContext;
-export type { IExampleModuleContext };
